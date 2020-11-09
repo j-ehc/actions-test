a1dae2c128fed690a8f8e25cc3c2c790dd20d8ba
Jesse <g00gleverse@gmail.com>
2020-11-09 13:11:37 -0800

    update

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SimpleTest {
+
+
+    @Test
+    public void test(){
+//        fail("not yet implemented");
+        assertEquals(5,5);
+    }
+
+}
